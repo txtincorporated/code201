cc45ae0c6714a129d68980991ddbefd9ce6ca2a5
Nathan P. Keene <npk@txtincorporated.com>
2016-05-17 06:12:40 -0700

    Update app.js, start index.html and sales-data.html, start stores.js to hold object and table functions, start CSS

--- Week2/pizza/app.js
+++ Week2/pizza/app.js
@@ -1,128 +1,131 @@
-  //VARIABLES
-  //--FOR USE IN FUNCTIONS
-  //Hold branch names in an array
-  var branches = ['Hillsboro', 'Pearl', 'DowntownPDX', 'Buckman', 'PDXairport', 'Clackamas'];
+//VARIABLES
+//--FOR USE IN FUNCTIONS
+//Hold branch names in an array
+var branches = ['Hillsboro', 'Pearl', 'DowntownPDX', 'Buckman', 'PDXairport', 'Clackamas'];
 
-  //Set min/max pizzas planned daily in three-hour blocks
-  var pizzaBounds = [[0,4],[0,7],[2,15],[15,35],[12,31],[5,20]];
+//Set min/max pizzas planned daily in three-hour blocks
+var pizzaBounds = [[0,4],[0,7],[2,15],[15,35],[12,31],[5,20]];
 
-  //Set min/max deliveries planned daily in three-hour blocks
-  var dlvyBounds = [[0,4],[0,4],[1,4],[3,8],[5,12],[5,11]];
+//Set min/max deliveries planned daily in three-hour blocks
+var dlvyBounds = [[0,4],[0,4],[1,4],[3,8],[5,12],[5,11]];
 
-  //--FOR POPULATING OBJECTS
-  //Store output from hrlyPies in an array
-  var dailyMade;
+//--FOR POPULATING OBJECTS
+//Store output from hrlyPies in an array
+var dailyMade;
 
-  //Store output from hrlyDelivs in an array
-  var dailyDelivs;
+//Store output from hrlyDelivs in an array
+var dailyDelivs;
 
-  //Store hourly drivers needed in an array
-  var driversPerHour;
+//Store hourly drivers needed in an array
+var driversPerHour;
 
- //--FOR POPULATING THE DOM
+//--FOR POPULATING THE DOM
+var piesServed;
+var thisWk = document.getElementById('served');
+thisWk.textContent = piesServed;
 
-
-  //FUNCTIONS
-  //Generate random data for pizzas made each hour
-  var hrlyPies = function (pizzaBounds) {
-    var piesPerHour = [];
-    for (i=0; i<6; i++) {
-      for (j=0; j<3; j++){
-        var number = Math.floor(Math.random () * (pizzaBounds[i][1] - pizzaBounds[i][0])) + pizzaBounds[i][0];
-        piesPerHour.push(number);
-      }
+//FUNCTIONS
+//Generate random data for pizzas made each hour
+var hrlyPies = function (pizzaBounds) {
+  var piesPerHour = [];
+  for (i=0; i<6; i++) {
+    for (j=0; j<3; j++){
+      var number = Math.floor(Math.random () * (pizzaBounds[i][1] - pizzaBounds[i][0])) + pizzaBounds[i][0];
+      piesPerHour.push(number);
     }
-    return piesPerHour;
-  };
-  var pizzasMade = hrlyPies(pizzaBounds);
-  console.log(pizzasMade);
+  }
+  return piesPerHour;
+};
+var pizzasMade = hrlyPies(pizzaBounds);
+console.log(pizzasMade);
 
-  //Generate daily pizzas ea. hr. for six days
-  var pizzaWeek = function () {
-    var dailyPies = [];
-    for (var i=0; i<6; i++) {
-      var wkly = hrlyPies(pizzaBounds);
-      dailyPies.push(wkly);
-      console.log(wkly);
+//Generate daily pizzas ea. hr. for six days
+var pizzaWeek = function () {
+  var dailyPies = [];
+  for (var i=0; i<6; i++) {
+    var wkly = hrlyPies(pizzaBounds);
+    dailyPies.push(wkly);
+    console.log(wkly);
+  }
+  console.log(dailyPies);
+  return dailyPies;
+};
+var weeksPizzas = pizzaWeek();
+console.log(weeksPizzas);
+dailyMade = weeksPizzas;
+
+//Build array of ea. day's totals
+var weeklyPizzas = function(arr) {
+  var weeklyTotals = [];
+  var dailyTotal = 0;
+  for (var i = 0; i<arr.length; i++) {//for ea. of six days
+    for (var j = 0; j<18; j++) {
+      dailyTotal += arr[i][j];
+      console.log(dailyTotal);
     }
-    console.log(dailyPies);
-    return dailyPies;
-  };
-  var weeksPizzas = pizzaWeek();
-  console.log(weeksPizzas);
-  dailyMade = weeksPizzas;
+  }
+  weeklyTotals.push(dailyTotal);
+  console.log(weeklyTotals);
+  return weeklyTotals;
+};
+var weeklyTotal = weeklyPizzas(weeksPizzas);
+console.log(weeklyTotal);
+piesServed = weeklyTotal;
 
-  //Build array of ea. day's totals
-  var weeklyPizzas = function(arr) {
-    var weeklyTotals = [];
-    var dailyTotal = 0;
-    for (var i = 0; i<arr.length; i++) {//for ea. of six days
-      for (var j = 0; j<18; j++) {
-        dailyTotal += arr[i][j];
-        console.log(dailyTotal);
-      }
+//Generate random data for deliveries made each hour
+var hrlyDelivs = function (dlvyBounds) {
+  var delivsPerHour = [];
+  for (i=0; i<6; i++) {
+    for (j=0; j<3; j++){
+      var number = Math.floor(Math.random () * (dlvyBounds[i][1] - dlvyBounds[i][0])) + dlvyBounds[i][0];
+      delivsPerHour.push(number);
     }
-    weeklyTotals.push(dailyTotal);
-    console.log(weeklyTotals);
-    return weeklyTotals;
-  };
-  var weeklyTotal = weeklyPizzas(weeksPizzas);
-  console.log(weeklyTotal);
+  }
+  return delivsPerHour;
+};
+var dlvyTrend = hrlyDelivs(dlvyBounds);
+console.log(dlvyTrend);
+dailyDelivs = dlvyTrend;
 
-  //Generate random data for deliveries made each hour
-  var hrlyDelivs = function (dlvyBounds) {
-    var delivsPerHour = [];
-    for (i=0; i<6; i++) {
-      for (j=0; j<3; j++){
-        var number = Math.floor(Math.random () * (dlvyBounds[i][1] - dlvyBounds[i][0])) + dlvyBounds[i][0];
-        delivsPerHour.push(number);
-      }
-    }
-    return delivsPerHour;
-  };
-  var dlvyTrend = hrlyDelivs(dlvyBounds);
-  console.log(dlvyTrend);
-  dailyDelivs = dlvyTrend;
+//Determine no. dlvy. drivers needed for each hour
+var hrlyDrivers = function (dTrend) {
+  var drvsNeeded = [];
+  for (var i = 0; i < dTrend.length; i++) {
+    var drvNeed = Math.round(dTrend[i]/3);
+    drvsNeeded.push(drvNeed);
+  }
+  return drvsNeeded;
+};
+var driverTrend = hrlyDrivers(dlvyTrend);
+console.log(driverTrend);
+driversPerHour = driverTrend;
 
-  //Determine no. dlvy. drivers needed for each hour
-  var hrlyDrivers = function (dTrend) {
-    var drvsNeeded = [];
-    for (var i = 0; i < dTrend.length; i++) {
-      var drvNeed = Math.round(dTrend[i]/3);
-      drvsNeeded.push(drvNeed);
-    }
-    return drvsNeeded;
-  };
-  var driverTrend = hrlyDrivers(dlvyTrend);
-  console.log(driverTrend);
-  driversPerHour = driverTrend;
-
-  //Add up total number of pizzas made for a day
-  // var pizzaTotal = function(arr) {
-  //   var pieCount = 0;
-  //   for (var i=0; i<arr.length; i++) {
-  //     pieCount += arr[i];
-  //   }
-  //   console.log(pieCount);
-  //   return pieCount;
-  // };
-  // console.log(pizzaTotal(pizzasMade));
-  //
-  //Add up total number of pizzas made for the week
-  // var wklyTotal = function(brr) {
-  //   console.log(brr);
-  //   var dailyTotal = 0;
-  //   for (var i=0; i<6; i++) {
-  //     dailyTotal = pizzaTotal(arr);
-  //     dailyPies.push(dailyTotal);
-  //     console.log(i);
-  //   }
-  //   console.log(dailyPies);
-  //   var wklyCount = 0;
-  //   for (j=0; j<6; j++) {
-  //     wklyCount += dailyPies[j];
-  //   }
-  //   console.log(wklyCount);
-  //   return wklyCount;
-  // };
-  // wklyTotal(pizzasMade);
+//Add up total number of pizzas made for a day
+// var pizzaTotal = function(arr) {
+//   var pieCount = 0;
+//   for (var i=0; i<arr.length; i++) {
+//     pieCount += arr[i];
+//   }
+//   console.log(pieCount);
+//   return pieCount;
+// };
+// console.log(pizzaTotal(pizzasMade));
+//
+//Add up total number of pizzas made for the week
+// var wklyTotal = function(brr) {
+//   console.log(brr);
+//   var dailyTotal = 0;
+//   for (var i=0; i<6; i++) {
+//     dailyTotal = pizzaTotal(arr);
+//     dailyPies.push(dailyTotal);
+//     console.log(i);
+//   }
+//   console.log(dailyPies);
+//   var wklyCount = 0;
+//   for (j=0; j<6; j++) {
+//     wklyCount += dailyPies[j];
+//   }
+//   console.log(wklyCount);
+//   return wklyCount;
+// };
+// wklyTotal(pizzasMade);
---
+++ Week2/pizza/stores.js
@@ -0,0 +1,13 @@
+var op_hours = ['8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm','12am','1am','2am'];
+
+var Hillsboro = {
+  name: Hillsboro,
+
+  pizzas:,//method to populate array from app.js
+  deliveries:,
+  driversNeeded:,
+  todaysPies:,
+  thisWksPies:,
+
+  storeStats:, //method to populate table from arrays above
+}
