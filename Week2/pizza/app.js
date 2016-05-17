5e5acc6f873762539f49ba3d4cee535313eb8242
Nathan P. Keene <npk@txtincorporated.com>
2016-05-16 22:08:15 -0700

    app.js working except for wkly total logic

---
+++ Week2/pizza/app.js
@@ -0,0 +1,79 @@
+//VARIABLES
+//Hold branch names in an array
+var branches = ['Hillsboro', 'Pearl', 'DowntownPDX', 'Buckman', 'PDXairport', 'Clackamas'];
+var dailyPies = [];
+
+//Set min/max pizzas planned daily in three-hour blocks
+var pizzaBounds = [[0,4],[0,7],[2,15],[15,35],[12,31],[5,20]];
+
+//Set min/max deliveries planned daily in three-hour blocks
+var dlvyBounds = [[0,4],[0,4],[1,4],[3,8],[5,12],[5,11]];
+
+//
+var driversPerHour = [];
+
+//Store hrly.
+var dailyMade = [];
+
+//FUNCTIONS
+//Generate random data for pizzas made each hour
+var hrlyPies = function (pizzaBounds) {
+  var piesPerHour = [];
+  for (i=0; i<6; i++) {
+    for (j=0; j<3; j++){
+      var number = Math.floor(Math.random () * (pizzaBounds[i][1] - pizzaBounds[i][0])) + pizzaBounds[i][0];
+      piesPerHour.push(number);
+    }
+  }
+  return piesPerHour;
+};
+var pizzasMade = hrlyPies(pizzaBounds);
+console.log(pizzasMade);
+
+//Generate random data for deliveries made each hour
+var hrlyDelivs = function (dlvyBounds) {
+  var delivsPerHour = [];
+  for (i=0; i<6; i++) {
+    for (j=0; j<3; j++){
+      var number = Math.floor(Math.random () * (dlvyBounds[i][1] - dlvyBounds[i][0])) + dlvyBounds[i][0];
+      delivsPerHour.push(number);
+    }
+  }
+  return delivsPerHour;
+};
+var dlvyTrend = hrlyDelivs(dlvyBounds);
+console.log(dlvyTrend);
+
+//Determine no. dlvy. drivers needed for each hour
+var hrlyDrivers = Math.round(hrlyDelivs/3);
+
+
+//Add up total number of pizzas made for the day
+var pizzaTotal = function(arr) {
+  var pieCount = 0;
+  for (var i=0; i<arr.length; i++) {
+    pieCount += arr[i];
+  }
+  //console.log(pieCount);
+  return pieCount;
+};
+//console.log(pizzaTotal(pizzasMade));
+
+//Add up total number of pizzas made for the week
+var wklyTotal = function(arr) {
+  console.log(arr);
+  var dailyTotal = 0;
+  for (var i=0; i<5; i++) {
+    dailyTotal = pizzaTotal(arr);
+    dailyPies.push(dailyTotal);
+    console.log(i);
+  }
+  console.log(dailyPies);
+  var wklyCount = 0;
+  for (j=0; j<5; j++) {
+    wklyCount += dailyPies[j];
+  }
+  console.log(wklyCount);
+  return wklyCount;
+};
+wklyTotal(pizzasMade);
